5ff4a3e0eae5b8f2b5ef7a8b43617aec90f051d7
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-04 17:22:30 +0530

    Components page has been created.

--- apps/docs/src/app/pages/components/components-home/components-home.component.ts
+++ apps/docs/src/app/pages/components/components-home/components-home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TngCard, TngAccordion, TngExpansionPanel, TngCardHeader, TngCardFooter } from "@tociva/tailng-ui/layout";
 
 @Component({
   standalone: true,
   selector: 'docs-components-home',
   templateUrl: './components-home.component.html',
+  imports: [TngCard, TngAccordion, TngExpansionPanel,TngCardHeader,TngCardFooter,RouterLink],
 })
 export class ComponentsHomeComponent {}
